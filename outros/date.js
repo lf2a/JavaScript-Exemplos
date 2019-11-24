5499ac4bbe9cd05e3626bb42a634ab006a8bb439
Luiz Filipy <luizfilipy.aa@outlook.com>
2019-11-23 23:28:06 -0300

    Create date.js

---
+++ outros/date.js
@@ -0,0 +1,15 @@
+var dateObj = new Date()
+
+console.log(`Now ${dateObj}`);
+console.log(dateObj.getTime())
+console.log(`${dateObj.getDate()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`)
+
+
+
+var newYear = new Date('December 31, 2019')
+console.log(newYear)
+
+
+
+var birthday = new Date(1999, 12, 07, 00, 00, 00);
+console.log(birthday)
